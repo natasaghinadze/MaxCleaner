45087761964a41b17a4214ff021e439bdc79c511
nata saghinadze <saginadzenata@gmail.com>
2025-10-04 13:35:33 +0400

    add roures.ts file and update main.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { CardComponentComponent } from './shared/components/card-component/card-component.component';
@@ -11,21 +10,15 @@ import { BuildingsEntrancesComponent } from './components/company-service/buildi
 import { GeneralArrangementComponent } from './components/company-service/general-arrangement/general-arrangement.component';
 import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: MainPageComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'card', component: CardComponentComponent },
   { path: 'repair', component: RepairComponent },
-  { path: 'windows', component: WindowsComponent},
-  { path: 'tidying-offices', component: TidyingOfficesComponent},
-  { path: 'private-houses', component: PrivateHousesComponent},
-  { path: 'buildings-entrances', component: BuildingsEntrancesComponent},
-  { path: 'general-arrangement', component: GeneralArrangementComponent}
+  { path: 'windows', component: WindowsComponent },
+  { path: 'tidying-offices', component: TidyingOfficesComponent },
+  { path: 'private-houses', component: PrivateHousesComponent },
+  { path: 'buildings-entrances', component: BuildingsEntrancesComponent },
+  { path: 'general-arrangement', component: GeneralArrangementComponent }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
